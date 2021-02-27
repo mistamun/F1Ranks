8f34f59166e66bd8e48b3afb742a22f8bcf0747b
Pedro Neto <cyb3neto@gmail.com>
2021-02-27 14:59:53 +0000

    custom users

--- F1Ranks/accounts/models.py
+++ F1Ranks/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employment(models.Model):
+    team = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+class RankedUser(User):
+    employment = models.ForeignKey(Employment, on_delete=models.CASCADE)
